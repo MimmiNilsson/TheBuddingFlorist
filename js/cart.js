7a9035043f13d3bfd7e7318908f232d37a4b923d
morgan <morgan.planeryd@gmail.com>
2021-12-10 13:51:31 +0100

    test1

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,18 @@
+/*===== ADMIN / NAVBAR ===== */
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+
+console.log(toggleButton);
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+
+
+console.log(navbarLinks);
+
+console.log(navbarLinks.classList)
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active');
+})
+
+
 var removeCartItemBottons = document.querySelector(".removeBtn")
 for (var i = 0; i < removeCartItemBottons.length; i++) {
     var button = removeCartItemBottons[i]
