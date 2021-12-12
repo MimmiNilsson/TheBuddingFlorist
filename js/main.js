bf032fc873c0c6fbe4b45f83efc320ac85fc664b
jaxhusen <jaxhusen@hotmail.com>
2021-12-12 11:59:45 +0100

    Joahnna

--- js/main.js
+++ js/main.js
@@ -10,8 +10,8 @@ PosX = new Array();
 PosY = new Array();
 StepX = new Array();
 StepY = new Array();
-StarSize=new Array();
- 
+StarSize=new Array(); 
+
 for (i = 0; i < Count; i++) {
 PosX[i] = Math.random()*Width;
 PosY[i] = Math.random()*Height;
@@ -20,25 +20,32 @@ StepY[i] =MinStep+Math.random()* MaxStep;
 StarSize[i]=MinFlake+Math.random() * MaxFlake;
  
 document.write("<div id='Obj" + i + "' style='position:absolute; left:0px; top:-20px; z-index:10000; visibility:hidden; color:#fff; font-weight:normal; font-family:Verdana; font-size:"+StarSize[i]+"pt'>•</div>");
-}
+} 
  
-function animate() {
-for (i = 0; i < Count; i++) {
-PosY[i] += StepY[i];
-PosX[i] += StepX[i];
+
+function snowFall() {
+    for (i = 0; i < Count; i++) {
+    PosY[i] += StepY[i];
+    PosX[i] += StepX[i];
+     
+    document.getElementById("Obj"+i).style.visibility = "visible";
+    if (PosY[i] > Width || PosX[i] > Height || PosX[i]<0) { // starta om från toppen av sidan när mitten nås
+    PosX[i] = Math.random()*Width; ///2
+    PosY[i] = -20;
+    StepX[i] = MinStep+Math.random()* -MaxStep;
+    StepY[i] = MinStep+Math.random()* MaxStep;
+    }
+     
+    document.getElementById("Obj"+i).style.top = PosY[i] + "px";
+    document.getElementById("Obj"+i).style.left = PosX[i] + "px";
+    }
+    setTimeout("snowFall()", 50);// hastighet
+    } 
  
-document.getElementById("Obj"+i).style.visibility = "visible";
-if (PosY[i] > Width || PosX[i] > Height || PosX[i]<0) { // starta om från toppen av sidan när mitten nås
-PosX[i] = Math.random()*Width; ///2
-PosY[i] = -20;
-StepX[i] = MinStep+Math.random()* -MaxStep;
-StepY[i] = MinStep+Math.random()* MaxStep;
-}
- 
-document.getElementById("Obj"+i).style.top = PosY[i] + "px";
-document.getElementById("Obj"+i).style.left = PosX[i] + "px";
-}
-setTimeout("animate()", 50);// hastighet
-}
- 
-setTimeout("animate()", 1000);
+ setTimeout("snowFall()", 1000);  
+
+
+
+
+/* 
+ element.onclick = snowFall(); */
