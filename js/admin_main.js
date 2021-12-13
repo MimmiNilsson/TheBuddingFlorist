a5251623a6d0140e8a1c6d3578aa50e9bb8885e8
MIMMI NILSSON <mimmizlnilsson@gmail.com>
2021-12-13 10:05:36 +0100

    admin

--- js/admin_main.js
+++ js/admin_main.js
@@ -11,3 +11,26 @@ console.log(navbarLinks.classList)
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active');
 })
+
+// empty array eller information från localstorage 
+const adminStock = JSON.parse( localStorage.getItem("adminStock")) || [];
+
+function adminAddStock(e) {
+    e.preventDefault();
+// vi läser in input fältet och sparar data in i localstorage
+const src= document.querySelector("#imgSrc").value;
+const productName= document.querySelector("#productName").value
+let productObj= {
+    productName:productName,
+    img:src
+}
+ productList.push(productObj)
+ localStorage.setItem("productList", JSON.stringify(productList))
+ //const oldProduct= localStorage.getItem("productList");
+ //productList.concat(oldProduct);
+// array av object 
+}
+
+
+
+document.querySelector("button").addEventListener("click" , addProduct);
