982ebbb25a1bb15e26924e859638496ecc884593
jaxhusen <jaxhusen@hotmail.com>
2021-12-13 15:42:30 +0100

    make it snow knapp check

--- js/snowflake.js
+++ js/snowflake.js
@@ -1,18 +1,26 @@
-const body = document.body;
- setInterval(createSnowFlake, 50); 
+window.addEventListener('load', () => {
+    console.log(document.querySelector("#makeItSnow"))
+    document.querySelector("#makeItSnow").addEventListener("click", dontBeAgrinch);})
 
-function createSnowFlake() {
-    const snow_flake = document.createElement('i');
-    snow_flake.classList.add('far');
-    snow_flake.classList.add('fa-snowflake');
-    snow_flake.style.right = Math.random() * window.innerWidth + 'px';
-    snow_flake.style.animationDuration = Math.random() * 25 + 15 + 's'; //between 5 och 12 seconds
-    snow_flake.style.opacity = Math.random();
-    snow_flake.style.fontSize = Math.random() * 10 + 5 + 'px'; 
+function dontBeAgrinch() {
+    const body = document.body;
+    setInterval(createSnowFlake, 50);
 
-    document.body.appendChild(snow_flake);
+    function createSnowFlake() {
+        const snow_flake = document.createElement('i');
+        snow_flake.classList.add('far');
+        snow_flake.classList.add('fa-snowflake');
+        snow_flake.style.right = Math.random() * window.innerWidth + 'px';
+        snow_flake.style.animationDuration = Math.random() * 25 + 15 + 's'; //between 5 och 12 seconds
+        snow_flake.style.opacity = Math.random();
+        snow_flake.style.fontSize = Math.random() * 10 + 5 + 'px';
 
-    setTimeout(() => {
-        snow_flake.remove();
-    }, 16000)
-} 
+        document.body.appendChild(snow_flake);
+
+        setTimeout(() => {
+            snow_flake.remove();
+        }, 16000)
+    }
+
+}
+
