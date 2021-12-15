e5a26831f37f47e8d1393ff7f5a4fb06605a74aa
jaxhusen <jaxhusen@hotmail.com>
2021-12-15 08:11:08 +0100

    bytt namn på bilderna

--- js/main.js
+++ js/main.js
@@ -1,36 +1,56 @@
+document.querySelector("button").addEventListener("click", addToCart);
 
-
-
-
-/* 
-const productList = JSON.parse( localStorage.getItem("productList")) || []; */
-/* 
-
-const addToList = [];
-document.querySelector("button").addEventListener("click", addProduct);
-
-function addProduct{
-    localStorage.setItem(, button)
-    
+function addToCart(){
+  
 }
 
 
 
-<script>
-function mySongTwo() {
-  localStorage.setItem("favsong", "<a href='https://song2.com'><h1>song2</h1></a>");
+
+
+
+
+
+
+
+
+
+/* försök ett hjärtfunktion
+document.querySelector("#far fa-heart").addEventListener("click", favouriteHeart);
+
+function favouriteHeart(){
+if("#far fa-heart".clicked==true){
 }
+}
+ */
 
 
-</script>
 
 
+/* försök två hjärtfunktion */
+ /* let heart = document.getElementById("far fa-heart");
+ heart.onclick = anotherHeart;
+
+ function anotherHeart(){
+//byter bild till ett ifyllt hjärta
+  document.getElementById("far fa-heart").innerHTML = ("fas fa-heart");
+ }
  */
 
 
 
 
 
+/*försök till hjärtfunktionen
+  const button = document.querySelector(".heartFunction");
+
+button.addEventListener("click", () => {
+  if (button.classList.contains("liked")) {
+    button.classList.remove("liked");
+  } else {
+    button.classList.add("liked");
+  }
+}); */
 
 
 
@@ -38,53 +58,9 @@ function mySongTwo() {
 
 
 
-/* 
 
-Width =1500; // ange bredden på sidan som ska snöas in...
-Height =2000; // ange höjden på sidan som ska snöas in...
-Count =25; // antal snöflingor OBS! ange inte för stort antal!
-MaxStep=3; // max steg i rörelsen
-MinStep=1; // min steg i rörelsen
-MaxFlake=10; // max storlek på snöflinga
-MinFlake=4; // min storlek på snöflinga
- 
-PosX = new Array();
-PosY = new Array();
-StepX = new Array();
-StepY = new Array();
-StarSize=new Array(); 
 
-for (i = 0; i < Count; i++) {
-PosX[i] = Math.random()*Width;
-PosY[i] = Math.random()*Height;
-StepX[i] =MinStep+Math.random()* -MaxStep;
-StepY[i] =MinStep+Math.random()* MaxStep;
-StarSize[i]=MinFlake+Math.random() * MaxFlake;
- 
-document.write("<div id='Obj" + i + "' style='position:absolute; left:0px; top:-20px; z-index:10000; visibility:hidden; color:#fff; font-weight:normal; font-family:Verdana; font-size:"+StarSize[i]+"pt'>•</div>");
-} 
- 
 
-function snowFall() {
-    for (i = 0; i < Count; i++) {
-    PosY[i] += StepY[i];
-    PosX[i] += StepX[i];
-     
-    document.getElementById("Obj"+i).style.visibility = "visible";
-    if (PosY[i] > Width || PosX[i] > Height || PosX[i]<0) { // starta om från toppen av sidan när mitten nås
-    PosX[i] = Math.random()*Width; ///2
-    PosY[i] = -20;
-    StepX[i] = MinStep+Math.random()* -MaxStep;
-    StepY[i] = MinStep+Math.random()* MaxStep;
-    }
-     
-    document.getElementById("Obj"+i).style.top = PosY[i] + "px";
-    document.getElementById("Obj"+i).style.left = PosX[i] + "px";
-    }
-    setTimeout("snowFall()", 50);// hastighet
-    } 
- 
- setTimeout("snowFall()", 1000);  */ 
 
  
 
@@ -96,28 +72,8 @@ function snowFall() {
 
 
 
-/*  document.getElementById("cirkel1").addEventListener("click", headerPic);
 
-function headerPic() {
-
-}
-
-// Get the modal
-var modal = document.getElementById('myModal');
-// Get the image and insert it inside the modal - use its 'alt' text as a caption
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById('img01');
-var captionText = document.getElementById('caption');
-img.onclick = function(){ modal.style.display = 'block';
-modalImg.src = this.src; captionText.innerHTML = this.alt; }
-// Get the  element that closes the modal
-var span = document.getElementsByClassName('close')[0];
-// When the user clicks on  (x), close the modal
-span.onclick = function() { modal.style.display = 'none'; }
-
-
-
-
+/*
 
 GRUND URL
 https://api.unsplash.com/photos/random/3
--- js/snowflake.js
+++ js/snowflake.js
@@ -11,7 +11,7 @@ function dontBeAgrinch() {
         snow_flake.classList.add('far');
         snow_flake.classList.add('fa-snowflake');
         snow_flake.style.right = Math.random() * window.innerWidth + 'px';
-        snow_flake.style.animationDuration = Math.random() * 25 + 15 + 's'; //between 5 och 12 seconds
+        snow_flake.style.animationDuration = Math.random() * 25 + 15 + 's'; 
         snow_flake.style.opacity = Math.random();
         snow_flake.style.fontSize = Math.random() * 10 + 5 + 'px';
 
@@ -19,7 +19,7 @@ function dontBeAgrinch() {
 
         setTimeout(() => {
             snow_flake.remove();
-        }, 16000)
+        }, 12500)
     }
 
 }
