c3ac7b2a738240ff0bcf30e9572618d5a517cff8
jaxhusen <jaxhusen@hotmail.com>
2021-12-15 18:50:18 +0100

    hjärtfunktionen funkaaaaar wohoooo

--- js/addToCart.js
+++ js/addToCart.js
@@ -1,119 +1,24 @@
-
-var shoppingCart = (function() {
-    // =============================
-    // Private methods and propeties
-    // =============================
-    cart = []; //localStorage eller
-    
-  // Constructor
-  function Item(name, price, count) {
-    this.name = name;
-    this.price = price;
-    this.count = count;
-  }
-
-  // Save cart
-  function saveCart() {
-    sessionStorage.setItem('shoppingCart', JSON.stringify(cart));
-  }
-
-  // Public methods and propeties
-  // =============================
-  var obj = {};
-  
-  // Add to cart
-  obj.addItemToCart = function(name, price, count) {
-    for(var item in cart) {
-      if(cart[item].name === name) {
-        cart[item].count ++;
-        saveCart();
-        return;
-      }
-    }
-    var item = new Item(name, price, count);
-    cart.push(item);
-    saveCart();
-  }
-
-  // Triggers / Events
-// ***************************************** 
-// Add item
-$('.add-to-cart').click(function(event) {
-    event.preventDefault();
-    var name = $(this).data('name');
-    var price = Number($(this).data('price'));
-    shoppingCart.addItemToCart(name, price, 1);
-    displayCart();
-  });
+let carts = document.querySelectorAll("button");
+let products =[ {
+    name: "Snowfall Bouquet"
+    /* tag:""
+    price: ""
+    inCart: 0,  */}
+]
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* var product=[];
-
-function fun(){
-  var x={};
-  x.price=document.getElementById('price').value;
-  x.title=document.getElementById('title').value;
-  product.push(x);
-    
-  var iDiv = document.createElement('div');
-  iDiv.id = product.length;
-  iDiv.className = 'block';
-  document.getElementsByTagName('body')[0].appendChild(iDiv);
-  
-  var para = document.createElement("span");
-  var node = document.createTextNode('Title: ' + x.title+' |                    ');
-  para.appendChild(node);
-
-  var element = document.getElementById(product.length);
-  element.appendChild(para);
-  
-  para = document.createElement("span");
-  node = document.createTextNode('Price: '+ x.price);
-  para.appendChild(node);
-  
-  element.appendChild(para);
+for (let i=0; i < carts.length; i++){
+    carts[i].addEventListener("click", () => {
+        cartNumbers();
+    })
 }
 
-Title:<input id="title" type="text">
-Price:<input id="price" type="number">
-<button onClick="fun()">Add to Cart </button>
+function cartNumbers(){
+    let productNumbers = localStorage.getItem('cartNumbers');
 
-<div>
-<p><b>Cart Info</b></p>
-</div> */
+
+    productNumbers = parseInt(productNumbers);
+    localStorage.setItem('cartNumbers', 1);
+
+}
--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
-document.querySelector("button").addEventListener("click", addToCart);
+const heart = document.querySelector(".heart");
+const animationHeart = document.querySelector(".animation-heart");
 
-function addToCart(){
-  
-}
+heart.addEventListener("click", () => {
+  animationHeart.classList.add('animation');
+  heart.classList.add('fill-color');
+});
 
 
+animationHeart.addEventListener("click", () => {
+animationHeart.classList.remove("animation");
+heart.classList.remove("fill-color");
+})
 
 
 
