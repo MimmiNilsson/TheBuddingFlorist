8293e0546a90691674aa871f3e45fbd44e7199d9
jaxhusen <jaxhusen@hotmail.com>
2021-12-16 15:24:12 +0100

    hjärtskiten funkar 100%

--- js/grinch.js
+++ js/grinch.js
@@ -7,61 +7,10 @@ const grinch = document.querySelector('#makeItSnow');
     if(this.style.backgroundColor === "brown"){
     grinch.style.backgroundColor = "green";
     grinch.value = "Be A Grinch: ";
-    
 } else {
         this.style.backgroundColor = "brown"; 
         grinch.value = "Make It Snow: ";
-        remove.dontBeAgrinch();
+        
     }
  } 
 
-
-/*function colorText()
- {
-     // If-satsen testar vilken färg det är på elementet som vi klickar på
-     if(this.style.backgroundColor == "brown")
-     {
-         // Om textfärgen är röd, så ändrar vi den till svart
-         this.style.backgroundColor = "green"; 
-     }
-     else
-     {
-         // Om textfärgen inte är röd, så ändrar vi den till röd
-         this.style.color = "brown"; 
-     }
- } 
-  
- function init()
- {
-     // Hämtar och sparar alla element som har klassen "text" (som en array)
-     var text = document.getElementsById("#makeItSnow");
-      
-     // Loopar igenom alla element med klassen "text" och lägger på event-handern "onclick"
-     for(var i = 0; i < text.length ;i++)
-     {
-         text[i].onclick = backgroundColorText;
-     }
- }
-  
- window.onload = init;
-
-
-
-
-
-
-
-
-
-
-
-
- // If the user clicks in the window, set the background color of <body> to yellow
-function grinchin() {
-    if (grinch === clicked) {
-        grinch.style.backgroundColor = 'green';
-        grinch.style.Text = 'Be a Grinch: ';
-    }
-  else{
-    createSnowFlake.remove();
-  }} */
--- js/main.js
+++ js/main.js
@@ -1,54 +1,18 @@
-const heart = document.querySelectorAll(".heart");
-const animationHeart = document.querySelectorAll(".card");
-
-window.addEventListener("load", () =>{
-  card.forEach(card => {
-    card.addEventListener("click", () =>{
-      card.classList.add("animation");
-    })
-  });
-})
-
-
-
-
-
-
-/* 
   const heart = document.querySelectorAll(".heart");
   const animationHeart = document.querySelectorAll(".animation-heart");
 
 
+  console.log(animationHeart.classList)
+
 heart.forEach( (heart =>  { //heart fill color + animation-heart animation
     heart.addEventListener("click", () => {
-    animationHeart.classList.add('animation');
-    heart.classList.add('fill-color');
+
+     
+    heart.classList.add('animation');
+    heart.classList.toggle('fill-color');
+    
   });
 })) 
-/* 
-heart.forEach( (heart =>  { //heart fill-color + animation-heart
-  animationHeart.addEventListener("click", () => {
-  heart.classList.add('animation');
-  heart.classList.add('fill-color');
-});
-}))  
-
-animationHeart.addEventListener("click", () => {
-animationHeart.classList.remove("animation");
-heart.classList.remove("fill-color");
-})  
- 
- 
-
-
-
-/* 
-function myFunction(x) {
-  x.classList.toggle.style.color = "red";
-
-} */
-
-
 
 /*
 GRUND URL
