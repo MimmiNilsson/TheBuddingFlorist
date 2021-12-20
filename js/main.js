6eb8f5066fd4ea4bbfe61e99d8db46fd9d3f8fd2
jaxhusen <jaxhusen@hotmail.com>
2021-12-20 14:09:10 +0100

    ingenting2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
   const heart = document.querySelectorAll(".heart");
   const animationHeart = document.querySelectorAll(".animation-heart");
 
-
   console.log(animationHeart.classList)
 
 heart.forEach( (heart =>  { //heart fill color + animation-heart animation
@@ -12,7 +11,7 @@ heart.forEach( (heart =>  { //heart fill color + animation-heart animation
     heart.classList.toggle('fill-color');
     
   });
-})) 
+}))
 
 /*
 GRUND URL
@@ -20,3 +19,32 @@ https://api.unsplash.com/photos/random/3
 https://api.unsplash.com/photos/random/3${count};
 https://api.unsplash.com/flowers/photos?query=office&per_page=3
  */
+// fetch()
+// res-urls 
+
+
+const response = await fetch("https://api.unsplash.com/flowers/photos?query=office&per_page=3")
+// fetch Unplash api och hämta random inom "flowers" bilder 
+
+async function fetchPicture() {
+   // hanterar vi promise
+    response.json().then( res=> {  //callback function i promise som extraherar response
+    // raw keys har bildadressen 
+    const img = document.createElement("img");
+    img.setAttribute("src", res.urls.large);
+    img.setAttribute("alt", "pictures from unsplash")
+    img.height="600"
+    img.width="400"
+    document.querySelector(".johanna").appendChild(img)
+  })  
+}
+
+async function fetchImg() {
+  const Urls="https://api.unsplash.com/photos/random?";
+  const client_id="client_id=StO9sFglhemQt_JdSRJytAsx971OWI5FKJa9B2yDfO4"
+  const response = await fetch(Urls+client_id);
+  response.json().then( res=> console.log(res.urls.small))
+}
+
+document.querySelector("button").addEventListener("click", fetchImg)
+
