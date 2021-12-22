7524f1c714f7f234e64b0efefa2bbaa38afae0bd
MIMMI NILSSON <mimmizlnilsson@gmail.com>
2021-12-22 21:09:06 +0100

    admin_login fixed and scss done for admin_main

--- js/admin_login.js
+++ js/admin_login.js
@@ -7,7 +7,7 @@ function validateAdmin(e) {
     const username = document.querySelector('#username').value;
     const password = document.querySelector('#password').value;
 
-    if(username == "admin" && password == "admin"){
+    if(username == "admin" && password == "grupp2"){
         window.location.href = "../html/admin_main.html";
     } else {
         alert("Login unsuccessful, please try again.")
